37868c8eb2670cd771db3d13dd90578168774e63
Krishnavamsi <111332913+krishnavamsi-22@users.noreply.github.com>
2024-05-20 20:42:46 +0530

    May 20th

---
+++ Modular Exponentiation for large numbers.java
@@ -0,0 +1,13 @@
+class Solution{
+    public long PowMod(long x, long n, long m){
+        long result = 1;
+        x = x % m;   
+        while (n > 0){
+            if ((n & 1) == 1)
+                result = (result * x) % m;
+            n = n >> 1;  
+            x = (x * x) % m;  
+        }
+        return result;
+    }
+}
