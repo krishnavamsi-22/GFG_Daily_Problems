990048cbb7d96fc451f7b871a5cb29ce9cf0dfaf
Krishnavamsi <111332913+krishnavamsi-22@users.noreply.github.com>
2024-10-12 15:12:27 +0530

    Oct 12th

---
+++ Two Smallests in every Subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pairWithMaxSum(int[] arr) {
+        // code here
+        int n = arr.length;
+        if(n<2){
+            return -1;
+        }
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0;i<n-1;i++){
+            int currSum = arr[i]+arr[i+1];
+            if(currSum>maxSum){
+                maxSum = currSum;
+            }
+        }
+        return maxSum;
+    }
+}
